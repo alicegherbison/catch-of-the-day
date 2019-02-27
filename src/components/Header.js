1d34b495d1eeeb7a08ef6d017d608346d4b7b43d
alicegherbison <alicegherbison@gmail.com>
2019-02-27 20:29:47 +0000

    Finish video 6

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="top">
+        <h1>Catch <span className="ofThe"><span className="of">of</span><span className="the">the</span></span> day</h1>
+        <h3 class="tagline">
+        <span>Fresh daily</span>
+      </h3>
+      </header>
+    )
+  }
+}
+
+export default Header;
