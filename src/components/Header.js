2cd75e457c3c69cd1ba30213c6857314fb74c049
alicegherbison <alicegherbison@gmail.com>
2019-02-28 12:51:17 +0000

    Finish video 7

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
       <header className="top">
         <h1>Catch <span className="ofThe"><span className="of">of</span><span className="the">the</span></span> day</h1>
         <h3 class="tagline">
-        <span>Fresh daily</span>
+        <span>{this.props.tagline}</span>
       </h3>
       </header>
     )
