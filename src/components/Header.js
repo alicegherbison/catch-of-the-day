7f5151ca9568062576352523e8c2751b5e0ac918
alicegherbison <alicegherbison@gmail.com>
2019-03-03 11:40:52 +0000

    Finish video 11

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = (props) => (
   <header className="top">
-  <h1>Catch <span className="ofThe"><span className="of">of</span><span className="the">the</span></span> day</h1>
-  <h3 class="tagline">
-  <span>{props.tagline}</span>
-  </h3>
+    <h1>Catch <span className="ofThe"><span className="of">of</span><span className="the">the</span></span> day</h1>
+    <h3 class="tagline">
+      <span>{props.tagline}</span>
+    </h3>
   </header>
-  );
-  
-  export default Header;
+);
+
+export default Header;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,29 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  };
+
+  myInput = React.createRef();
+
+  goToStore(event) {
+    event.preventDefault();
+    
+  };
   render() {
     return (
       <Fragment>
-      <form action="" className="store-selector">
-      <h2>What store would you like to visit?</h2>
-      <input type="text" placeholder="Store name" required />
-      <button type="submit">Visit store →</button>
-      </form>
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
+          <h2>What store would you like to visit?</h2>
+          <input type="text" placeholder="Store name" defaultValue = {getFunName()} ref={this.myInput} required />
+          <button type="submit">Visit store →</button>
+        </form>
       </Fragment>
-      )
-    }
+    )
   }
-  
-  export default StorePicker;
+}
+
+export default StorePicker;
