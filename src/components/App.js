824298eddac08a7246af847f5bdf746c8ab75f65
alicegherbison <alicegherbison@gmail.com>
2019-03-12 12:18:38 +0000

    Finish video 20

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
   componentDidMount() {
     const { params } = this.props.match;
     const localStorageRef = localStorage.getItem(params.storeID);
-    if(localStorageRef) {
+    if (localStorageRef) {
       this.setState({ order: JSON.parse(localStorageRef) });
     }
     this.ref = base.syncState(`${params.storeID}/fishes`, {
@@ -30,11 +30,17 @@ class App extends React.Component {
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
-  } 
+  }
 
   addFish = (fish) => {
-    const fishes = {...this.state.fishes};
+    const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
+  updateFish = (key, updatedFish) => {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
     this.setState({ fishes });
   };
 
@@ -43,7 +49,7 @@ class App extends React.Component {
   };
 
   addToOrder = (key) => {
-    const order = {...this.state.order};
+    const order = { ...this.state.order };
     order[key] = order[key] + 1 || 1;
     this.setState({ order });
   }
@@ -57,11 +63,16 @@ class App extends React.Component {
             {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          addFish={this.addFish}
+          updateFish={this.updateFish}
+          loadSampleFishes={this.loadSampleFishes}
+          fishes={this.state.fishes}
+        />
       </div>
-      )
-    }
+    )
   }
-  
-  export default App;
+}
+
+export default App;
